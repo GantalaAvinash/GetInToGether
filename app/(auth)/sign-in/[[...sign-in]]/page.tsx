783e9f92ccf99190d26f89fa7657dfa27b07db27
gantalaavinash <gantalaavinash@gmail.com>
2024-09-23 11:30:20 +0530

    connected to mongodb

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -1,21 +1,11 @@
-'use client';
-
-import { SignIn } from '@clerk/nextjs';
-import Link from 'next/link';
+import { SignIn } from "@clerk/nextjs";
 
 const SignInPage = () => {
-
-  return (
-    <main className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-      <SignIn />
-      <div className="text-sm mt-2 flex">
-        <p>Forgot Password?</p>
-        <Link href="/forget-password" className="text-blue-400 ml-2">
-          Reset here
-        </Link>
-      </div>
-    </main>
-  );
+    return (
+        <main className="auth-page">
+          <SignIn />
+        </main>
+    );
 };
 
 export default SignInPage;
