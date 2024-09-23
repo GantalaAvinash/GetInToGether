89ec54e7c9626217b64d0043d5d62c327273ff30
gantalaavinash <gantalaavinash@gmail.com>
2024-09-23 20:51:46 +0530

    api user route deleted

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,24 +1,7 @@
-'use client';
-
-
-import { Button } from "@/components/ui/button";
-// import { Button } from "@/components/ui/button";
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-
 export default function Home() {
   return (
-    <div className="container">
-    <h1 className="text-3xl text-center font-bold mb-2">
-      Get In Together
-    </h1>
-      <SignedOut>
-        <Button>
-          <SignInButton />
-        </Button>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+    <div className="flex items-center justify-center h-full">
+      <div className="text-2xl">Home Page</div>
     </div>
-  )
+  );
 }
