ece0ab3b5f7f945fa9a743caec3dad2e44bbdcf1
gantalaavinash <gantalaavinash@gmail.com>
2024-09-24 23:28:37 +0530

    clerk middleware changes

--- middleware.ts
+++ middleware.ts
@@ -11,7 +11,6 @@ export default clerkMiddleware((auth, req) => {
 export const config = {
   matcher: [
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
